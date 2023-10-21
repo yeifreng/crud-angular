314e3d10d052beec496849652eec714aeca4a820
Yeifreng <yeifrenjose@gmail.com>
2023-10-21 11:38:31 -0500

    creacion de routes para acceder a los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
+import { AppComponent } from './app.component';
+import { FormularioEmpleadosComponent } from './formulario-empleados/formulario-empleados.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:AppComponent},
+  {path:'agregar', component:FormularioEmpleadosComponent},
+  {path:'empleados', component:ListaEmpleadosComponent},
+  {path:'**', component:ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
